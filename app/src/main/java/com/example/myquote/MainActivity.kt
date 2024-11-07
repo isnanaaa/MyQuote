231c4b30c2a4a87755f966fcdc816c6fa5a72f55
isnanaaa <isnan238@gmail.com>
2024-11-07 15:30:07 +0700

    first commit

--- app/src/main/java/com/example/myquote/MainActivity.kt
+++ app/src/main/java/com/example/myquote/MainActivity.kt
@@ -36,7 +36,6 @@ class MainActivity : AppCompatActivity() {
         }
 
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.activity_main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
